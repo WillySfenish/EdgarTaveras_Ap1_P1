6cd7e772d2b197891fa4fbda6c032688e0308e61
Edgar Taveras <edgartopia@gmail.com>
2024-09-23 21:15:49 -0400

    se creo el modelo de registro

---
+++ Models/Registros.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EdgarTaveras_Ap1_P1.Models
+{
+    public class Registros
+    {
+        [Key]
+        public int RegistroId { get; set; }
+
+        public string? RegistroName { get; set; }
+    }
+}
