ed8495ff8d8149c3c8641647ff77f1394b270e2a
Edgar Taveras <edgartopia@gmail.com>
2024-09-23 21:14:50 -0400

    Se creo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using EdgarTaveras_Ap1_P1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EdgarTaveras_Ap1_P1.DAL;
+
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options)
+    {
+
+    }
+    public DbSet<Registros> Registros { get; set; }
+}
+
